428daf21647a5a87a9e8fe4428d5c856c30b1cb0
yashyk <7ykore@gmail.com>
2025-08-04 19:52:14 +0530

    set env with appwrite

---
+++ project/megaProject/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  return (
+    <>
+    <h1>I here to develop</h1>
+    </>
+  )
+}
+
+export default App
